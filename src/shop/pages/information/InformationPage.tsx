26c1049176f603a6234662c7eff83340f747a0ff
Hector Flores <hectordev007@gmail.com>
2025-10-17 14:09:25 -0300

    Category-page-06

--- src/shop/pages/information/InformationPage.tsx
+++ src/shop/pages/information/InformationPage.tsx
@@ -1,19 +1,19 @@
 
 export const InformationPage = () => {
   return (
-    <div>
-      <div>
-        <h2>Sobre las aceitunas</h2>
-        <p>
-          Estudios recientes destacan los múltiples beneficios para la salud de las aceitunas, especialmente las negras, que se han mostrado particularmente efectivas en la protección cardiovascular gracias a sus altos niveles de polifenoles y vitamina E.
- Un estudio de la Universidad de Jaén ha demostrado que las aceitunas de mesa fermentadas al estilo natural son una fuente potencial de bacterias probióticas, como Lactobacillus pentosus, que pueden resistir condiciones adversas del tracto digestivo y ayudar a prevenir infecciones al desplazar patógenos.
+    <div className="p-4">
+      <div className="text-gray-200 bg-purple-200/20 rounded-2xl p-4 shadow-2xl">
 
-Las aceitunas, tanto verdes como negras, son ricas en ácidos grasos monoinsaturados, principalmente ácido oleico (omega-9), que contribuyen a reducir el colesterol LDL ("colesterol malo") y protegen contra enfermedades cardiovasculares.
-Las aceitunas negras, al madurar más y someterse a un proceso de fermentación más prolongado, presentan una mayor concentración de antioxidantes como el hidroxitirosol y el tirosol, lo que potencia sus propiedades protectoras frente a la oxidación del colesterol y la inflamación.
-El consumo regular de aceitunas puede mejorar la función cognitiva y reducir el riesgo de enfermedades neurodegenerativas como el Alzheimer, gracias a sus compuestos antioxidantes y grasas saludables.
-Las aceitunas son una fuente de fibra, hierro y vitaminas del grupo B y E, lo que favorece la digestión, ayuda a prevenir la anemia y protege las células del daño oxidativo.
-Aunque son ricas en sodio debido al proceso de conservación en salmuera, no engordan si se consumen con moderación; se recomienda un consumo diario de entre cinco y diez aceitunas negras.
-        </p>
+        <h2 className="text-2xl text-center pb-4">Sobre las aceitunas</h2>
+
+        <p>Estudios recientes destacan los múltiples beneficios para la salud de las aceitunas, especialmente las negras, que se han mostrado particularmente efectivas en la protección cardiovascular gracias a sus altos niveles de polifenoles y vitamina E.</p>
+        <p>Un estudio de la Universidad de Jaén ha demostrado que las aceitunas de mesa fermentadas al estilo natural son una fuente potencial de bacterias probióticas, como Lactobacillus pentosus, que pueden resistir condiciones adversas del tracto digestivo y ayudar a prevenir infecciones al desplazar patógenos.</p>
+        <p>Las aceitunas, tanto verdes como negras, son ricas en ácidos grasos monoinsaturados, principalmente ácido oleico (omega-9), que contribuyen a reducir el colesterol LDL ("colesterol malo") y protegen contra enfermedades cardiovasculares.</p>
+        <p>Las aceitunas negras, al madurar más y someterse a un proceso de fermentación más prolongado, presentan una mayor concentración de antioxidantes como el hidroxitirosol y el tirosol, lo que potencia sus propiedades protectoras frente a la oxidación del colesterol y la inflamación.</p>
+        <p>El consumo regular de aceitunas puede mejorar la función cognitiva y reducir el riesgo de enfermedades neurodegenerativas como el Alzheimer, gracias a sus compuestos antioxidantes y grasas saludables.</p>
+        <p>Las aceitunas son una fuente de fibra, hierro y vitaminas del grupo B y E, lo que favorece la digestión, ayuda a prevenir la anemia y protege las células del daño oxidativo.</p>
+        <p>Aunque son ricas en sodio debido al proceso de conservación en salmuera, no engordan si se consumen con moderación; se recomienda un consumo diario de entre cinco y diez aceitunas negras.</p>
+
       </div>
     </div>
   )
